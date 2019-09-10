9021fa38176d55a1d85abd2a1e34d854881f7062
wangcong18 <wangcong18@xiaomi.com>
2019-09-10 22:24:43 +0800

    add thrift example

---
+++ main.cpp
@@ -0,0 +1,39 @@
+//
+// Created by wangcong on 2019/9/10.
+//
+
+#include "gen-cpp/Serv.h"
+#include "gen-cpp/student_constants.h"
+#include "gen-cpp/student_types.h"
+#include <thrift/transport/TSocket.h>
+#include <thrift/transport/TBufferTransports.h>
+#include <thrift/protocol/TBinaryProtocol.h>
+
+using namespace apache::thrift;
+using namespace apache::thrift::protocol;
+using namespace apache::thrift::transport;
+
+using std::shared_ptr;
+
+int main(int argc, char** argv) {
+    shared_ptr<TSocket> socket(new TSocket("localhost", 9090));
+    shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+    shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+
+    transport->open();
+
+    Student s;
+    s.sno = 123;
+    s.sname = "wangcong";
+    s.ssex = 1;
+    s.sage = 18;
+
+    ServClient client(protocol);
+    printf("sno = %d, sname = %s, ssex = %d, sage = %d\n", s.sno, s.sname.c_str(), s.ssex, s.sage);
+    client.put(s);
+    std::string strname = "";
+    client.scall(strname, s);
+    printf("icall = %d, scall = %s\n", client.icall(s), strname.c_str());
+    transport->close();
+    return 0;
+}
