40e2277ac52d89bd2ca7ef952afc3329947e5e74
WingC <1018957763@qq.com>
2019-09-11 00:43:51 +0800

    修改CMakeLists.txt包含thrift动态连接库

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <thrift/transport/TSocket.h>
 #include <thrift/transport/TBufferTransports.h>
 #include <thrift/protocol/TBinaryProtocol.h>
+#include <memory>
 
 using namespace apache::thrift;
 using namespace apache::thrift::protocol;
@@ -16,7 +17,7 @@ using namespace apache::thrift::transport;
 using std::shared_ptr;
 
 int main(int argc, char** argv) {
-    shared_ptr<TSocket> socket(new TSocket("localhost", 9090));
+    shared_ptr<TSocket> socket(new TSocket("localhost", 9999));
     shared_ptr<TTransport> transport(new TBufferedTransport(socket));
     shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
 
