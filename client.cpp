6416f588c9103db3583b0347b83a742c5a4cc739
wangcong18 <wangcong18@xiaomi.com>
2019-09-11 15:57:48 +0800

    modify examples

--- client.cpp
+++ client.cpp
@@ -2,39 +2,71 @@
 // Created by wangcong on 2019/9/10.
 //
 
-#include "gen-cpp/Serv.h"
-#include "gen-cpp/student_constants.h"
-#include "gen-cpp/student_types.h"
-#include <thrift/transport/TSocket.h>
-#include <thrift/transport/TBufferTransports.h>
+#include <iostream>
+
 #include <thrift/protocol/TBinaryProtocol.h>
-#include <memory>
+#include <thrift/transport/TSocket.h>
+#include <thrift/transport/TTransportUtils.h>
+
+#include "gen-cpp/Calculator.h"
 
 using namespace apache::thrift;
 using namespace apache::thrift::protocol;
 using namespace apache::thrift::transport;
 
-using std::shared_ptr;
+using namespace tutorial;
+using namespace shared;
+
+class EnsureClose {
+private:
+    std::shared_ptr<TTransport>* _transport = nullptr;
+public:
+    explicit EnsureClose(std::shared_ptr<TTransport>& transport) : _transport(&transport) {}
+    ~EnsureClose() {
+        if (_transport != nullptr) {
+            (*_transport)->close();
+        }
+    }
+};
 
 int main(int argc, char** argv) {
-    shared_ptr<TSocket> socket(new TSocket("localhost", 9090));
-    shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-    shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+    std::shared_ptr<TSocket> socket(new TSocket("localhost", 9090));
+    std::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
+    std::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
+    CalculatorClient client(protocol);
 
-    transport->open();
+    try {
+        EnsureClose ensureClose(transport);
+        transport->open();
+        client.ping();
+        std::cout << "ping()" << std::endl;
 
-    Student s;
-    s.sno = 123;
-    s.sname = "wangcong";
-    s.ssex = 1;
-    s.sage = 18;
+        std::cout << "1 + 1 = " << client.add(1, 1) << std::endl;
 
-    ServClient client(protocol);
-    printf("sno = %d, sname = %s, ssex = %d, sage = %d\n", s.sno, s.sname.c_str(), s.ssex, s.sage);
-    client.put(s);
-    std::string strname = "";
-    client.scall(strname, s);
-    printf("icall = %d, scall = %s\n", client.icall(s), strname.c_str());
-    transport->close();
+        Work work;
+        work.op = Operation::DIVIDE;
+        work.num1 = 1;
+        work.num2 = 0;
+
+        try {
+            client.calculate(1, work);
+            std::cout << "Whoa? We can divide by zero!" << std::endl;
+        } catch (const InvalidOperation& op) {
+            std::cout << "InvalidOperation: " << op.why << std::endl;
+        }
+
+        work.op = Operation::SUBTRACT;
+        work.num1 = 15;
+        work.num2 = 10;
+        int32_t diff = client.calculate(1, work);
+        std::cout << "15 - 10 = " << diff << std::endl;
+
+        SharedStruct ss;
+        client.getStruct(ss, 1);
+        std::cout << "Received Log: " << ss << std::endl;
+    } catch (const TException& tx) {
+        std::cout << "ERROR: " << tx.what() << std::endl;
+    }
+
     return 0;
 }
