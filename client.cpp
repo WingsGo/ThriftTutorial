bf3ee8fc79e5017064a4fa6dc8f7018940dc9862
wangcong18 <wangcong18@xiaomi.com>
2019-09-11 10:45:52 +0800

    添加server端代码

--- client.cpp
+++ client.cpp
@@ -17,7 +17,7 @@ using namespace apache::thrift::transport;
 using std::shared_ptr;
 
 int main(int argc, char** argv) {
-    shared_ptr<TSocket> socket(new TSocket("localhost", 9999));
+    shared_ptr<TSocket> socket(new TSocket("localhost", 9090));
     shared_ptr<TTransport> transport(new TBufferedTransport(socket));
     shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
 
