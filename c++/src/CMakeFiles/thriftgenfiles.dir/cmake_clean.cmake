4eb21a2a22121732c18adf58065769000861fa50
wangcong18 <wangcong18@xiaomi.com>
2019-10-08 11:13:38 +0800

    add java code

---
+++ c++/src/CMakeFiles/thriftgenfiles.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "gen-cpp/Calculator.cpp"
+  "gen-cpp/shared_constants.cpp"
+  "gen-cpp/shared_types.cpp"
+  "gen-cpp/SharedService.cpp"
+  "gen-cpp/tutorial_constants.cpp"
+  "gen-cpp/tutorial_types.cpp"
+  "CMakeFiles/thriftgenfiles.dir/gen-cpp/Calculator.cpp.o"
+  "CMakeFiles/thriftgenfiles.dir/gen-cpp/shared_constants.cpp.o"
+  "CMakeFiles/thriftgenfiles.dir/gen-cpp/shared_types.cpp.o"
+  "CMakeFiles/thriftgenfiles.dir/gen-cpp/SharedService.cpp.o"
+  "CMakeFiles/thriftgenfiles.dir/gen-cpp/tutorial_constants.cpp.o"
+  "CMakeFiles/thriftgenfiles.dir/gen-cpp/tutorial_types.cpp.o"
+  "libthriftgenfiles.pdb"
+  "libthriftgenfiles.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/thriftgenfiles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
